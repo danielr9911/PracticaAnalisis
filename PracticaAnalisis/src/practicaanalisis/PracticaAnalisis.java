894a9b468305f5f4c2754c961eb97f4e16c3627e
Daniel Rendon <dani-9911@hotmail.com>
2018-04-16 12:35:34 -0500

    Cambios para ejecucion

--- PracticaAnalisis/src/practicaanalisis/PracticaAnalisis.java
+++ PracticaAnalisis/src/practicaanalisis/PracticaAnalisis.java
@@ -5,6 +5,8 @@
  */
 package practicaanalisis;
 
+import interfaz.Home;
+
 /**
  *
  * @author Daniel Rendon M
@@ -16,6 +18,12 @@ public class PracticaAnalisis {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Starting...");
+        Home home = new Home();
+        home.setVisible(true);
+        home.setSize(1024,768);
+        home.setResizable(false);
+        home.setLocationRelativeTo(null);
     }
     
 }
