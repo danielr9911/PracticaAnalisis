a052b956b2a287e7054ecc8c0c2da29cd21f0b06
Pedro Salzedo <pedro.djzedo@gmail.com>
2018-04-21 14:28:32 -0500

    Punto Fijo

--- PracticaAnalisis/src/metodos/PuntoFijo.py
+++ PracticaAnalisis/src/metodos/PuntoFijo.py
@@ -1,50 +1,109 @@
+# -*- coding: utf-8 -*-
 from py_expression_eval import Parser
 
-def f(i):
-    sFuncion = "x*log(x)-x"
-    parser = Parser()
-    funcion = parser.parse(sFuncion)
-    res = funcion.evaluate({"x": i})
-    return res
+class Metodos():
+    __gui = None
+    mensaje = ""
+    data = []
 
-def df(i):
-    sFuncion = "log(x)"
-    parser = Parser()
-    funcion = parser.parse(sFuncion)
-    res = funcion.evaluate({"x": i})
-    return res
+    def __init__(self, gui):
+        self.__gui = gui
 
-def ddf(i):
-    sFuncion = "1/x"
-    parser = Parser()
-    funcion = parser.parse(sFuncion)
-    res = funcion.evaluate({"x": i})
-    return res
+    def funcion(self, func, i):
+        parser = Parser()
+        funcion = parser.parse(func)
+        res = funcion.evaluate({"x": i})
+        return res
 
-def g(i):
-    sFuncion = "x*log(x)"
-    parser = Parser()
-    funcion = parser.parse(sFuncion)
-    res = funcion.evaluate({"x": i})
-    return res
+    def biseccion(self, f, xi, xs, tol, iter, err):
+        fxi = self.funcion(f,xi)
+        fxs = self.funcion(f, xs)
+        if fxi == 0:
+            self.mensaje = "%f es una raiz" % xi
+        elif fxs == 0:
+            self.mensaje = "%f es una raiz" % xs
+        elif fxi * fxs < 0:
+            xm = (xi + xs) / 2
+            fxm = self.funcion(f, xm)
+            cont = 1
+            error = tol + 1
+            fila = []
+            fila.append(cont - 1)
+            fila.append(xi)
+            fila.append(xs)
+            fila.append(xm)
+            fila.append(fxm)
+            fila.append(0)
+            self.data.append(fila)
+            while fxm != 0 and error > tol and cont < iter:
+                if fxi * fxm < 0:
+                    xs = xm
+                    fxs = fxm
+                else:
+                    xi = xm
+                    fxi = fxm
+                xaux = xm
+                xm = (xi + xs) / 2
+                fxm = self.funcion(f, xm)
+                if err == 0:
+                    error = abs(xm - xaux)
+                else:
+                    error = abs((xm - xaux) / xm)
+                cont = cont + 1
+                fila = []
+                fila.append(cont - 1)
+                fila.append(xi)
+                fila.append(xs)
+                fila.append(xm)
+                fila.append(fxm)
+                fila.append(error)
+                self.data.append(fila)
+            if fxm == 0:
+                self.mensaje = "%f es una raiz" % xm
+            elif error < tol:
+                self.mensaje = "%f es aproximacion a una raiz con una tolerancia de %f" % (xm, tol)
+            else:
+                self.mensaje = "Fracaso en %d iteraciones" % iter
+        else:
+            self.mensaje = "El intervalo es inadecuado"
+        return (self.mensaje, self.data)
+    
+    def punto_fijo(self, f, g, x0, iter, tol, err):
+        self.mensaje = ""
+        self.data = []
+        yn = self.funcion(f,x0)
+        contador = 0
+        error = tol + 1
+        fila = [] #iter, xn, fXn, error
+        fila.append(contador)
+        fila.append(x0)
+        fila.append(yn)
+        fila.append(0)
+        self.data.append(fila)
+        while yn != 0 and error > tol and contador < iter :
+            xn = self.funcion(g, x0)
+            yn = self.funcion(f, xn)
+            if err == 0:
+                error = abs(xn - x0)
+            else:
+                error = abs((xn - x0)/xn)
+            x0 = xn
+            contador = contador + 1
+            fila.append(contador)
+            fila.append(x0)
+            fila.append(yn)
+            fila.append(error)
+            self.data.append(fila)
+        if yn == 0:
+            self.mensaje = "%f es raíz" %x0
+        elif error < tol :
+            self.mensaje = "%f es aproximacion con una tol: %f" %(x0, tol)
+        else:
+            self.mensaje = "El metodo fracaso en %d iteraciones" %iter
+        
+        return (self.mensaje, self.data)
+
+if __name__=='__main__':
+    Metodos.punto_fijo(Metodos(None),"x*exp(x) - x^2 - 5*x - 3","(x*exp(x) - x^2 - 3) / 5", -0.5, 11, 0.0005, 0)
 
 
-def punto_fijo(xa, niter, tolerancia):
-    yn = f(xa)
-    contador = 0
-    error = tolerancia + 1
-    while yn != 0 and error > tolerancia and contador < niter :
-        xn = g(xa)
-        yn = f(xn)
-        error = abs(xn - xa)
-        xa = xn
-        contador = contador + 1
-    if yn == 0:
-        print("%d es raíz" % xa)
-    elif error < tolerancia :
-        print("%d es aproximación con una tolerancia: %d", (xa, tolerancia))
-    else:
-        print("El método fracasó en %d iteraciones", niter)
-
-if __name__ == '__main__':
-    print (punto_fijo(xa=2.5,tolerancia=0.0005,niter=11))
