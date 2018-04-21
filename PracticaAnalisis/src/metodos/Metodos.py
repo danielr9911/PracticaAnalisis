465e3b3e29b512e90f4ee2d01a3af728e78606da
Daniel Rendon <dani-9911@hotmail.com>
2018-04-21 14:18:15 -0500

    Agrego Raices multiples python

--- PracticaAnalisis/src/metodos/Metodos.py
+++ PracticaAnalisis/src/metodos/Metodos.py
@@ -16,6 +16,8 @@ class Metodos:
         return res
 
     def biseccion(self, f, xi, xs, tol, iter, err):
+        self.mensaje = ""
+        self.data = []
         fxi = self.funcion(f,xi)
         fxs = self.funcion(f, xs)
         if fxi == 0:
@@ -67,3 +69,51 @@ class Metodos:
         else:
             self.mensaje = "El intervalo es inadecuado"
         return (self.mensaje, self.data)
+
+    def raicesMultiples(self, f, df, ddf, x0, tol, iter, err):
+        self.mensaje = ""
+        self.data = []
+        fx = self.funcion(f,x0)
+        dfx = self.funcion(df, x0)
+        ddfx = self.funcion(ddf,x0)
+        cont = 0
+        error = tol + 1
+        fila = []
+        fila.append(cont)
+        fila.append(x0)
+        fila.append(fx)
+        fila.append(dfx)
+        fila.append(ddf)
+        fila.append(0)
+        self.data.append(fila)
+        while fx != 0 and error > tol and cont < iter:
+            x1 = x0 - ((fx *dfx) / ((dfx**2) - (fx*ddfx)))
+            fx = self.funcion(f,x1)
+            dfx = self.funcion(df,x1)
+            ddfx = self.funcion(ddf, x1)
+            if err == 0:
+                error = abs(x1 - x0)
+            else:
+                error = abs((x1 - x0) / x1)
+            x0 = x1
+            cont = cont + 1
+            fila = []
+            fila.append(cont)
+            fila.append(x0)
+            fila.append(fx)
+            fila.append(dfx)
+            fila.append(ddf)
+            fila.append(error)
+            self.data.append(fila)
+        if fx == 0:
+            self.mensaje= "%f es una raíz" %x0
+        elif error < tol:
+            self.mensaje = "%f es aproximación a una raíz con una tolerancia de %f" % (x1, tol)
+        else:
+            self.mensje = "Fracaso en %d iteraciones" %iter
+        print(self.mensaje, self.data)
+        return (self.mensaje, self.data)
+
+if __name__ == '__main__':
+    Metodos.raicesMultiples(Metodos(None), 'x^4-18*x^2+81', '(4*x^3)-(36*x)', '(12*x^2)-36', -2.5, 0.00000005, 25, 0)
+
