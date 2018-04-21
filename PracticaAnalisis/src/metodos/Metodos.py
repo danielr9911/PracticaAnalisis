7346eed3a1093af0e56ca46be584a3f0281beb12
Daniel Rendon <dani-9911@hotmail.com>
2018-04-21 17:43:45 -0500

    regla falsa python

--- PracticaAnalisis/src/metodos/Metodos.py
+++ PracticaAnalisis/src/metodos/Metodos.py
@@ -70,6 +70,59 @@ class Metodos:
             self.mensaje = "El intervalo es inadecuado"
         return (self.mensaje, self.data)
 
+    def reglaFalsa(self, f, xi, xs, tol, iter, err):
+        fxi = self.funcion(f, xi)
+        fxs = self.funcion(f, xs)
+        if fxi == 0:
+            self.mensaje = "%f es una raíz" % xi
+        elif fxs == 0:
+            self.mensaje = "%f es una raíz" % xs
+        elif fxi * fxs < 0:
+            xm = xi - ((fxi) * (xs - xi) / (fxs - fxi))
+            fxm = self.funcion(f, xm)
+            cont = 1
+            error = tol + 1
+            fila = []
+            fila.append(cont - 1)
+            fila.append(xi)
+            fila.append(xs)
+            fila.append(xm)
+            fila.append(fxm)
+            fila.append(0)
+            self.data.append(fila)
+            while fxm != 0 and error > tol and cont < iter:
+                if fxi * fxm < 0:
+                    xs = xm
+                    fxs = fxm
+                else:
+                    xi = xm
+                    fxi = fxm
+                xaux = xm
+                xm = xi - ((fxi) * (xs - xi) / (fxs - fxi))
+                fxm = self.funcion(xm)
+                if err == 0:
+                    error = abs(xm - xaux)
+                else:
+                    error = abs((xm - xaux) / xm)
+                cont = cont + 1
+                fila = []
+                fila.append(cont - 1)
+                fila.append(xi)
+                fila.append(xs)
+                fila.append(xm)
+                fila.append(fxm)
+                fila.append(error)
+                self.data.append(fila)
+            if fxm == 0:
+                self.mensaje = "%f es una raiz" % xm
+            elif error < tol:
+                self.mensaje = "%f es aproximacion a una raiz con una tolerancia de %f " % (xm, tol)
+            else:
+                self.mensaje = "Fracaso en %d iteraciones" % iter
+        else:
+            self.mensaje = "El intervalo es inadecuado"
+        return (self.mensaje, self.data)
+
     def raicesMultiples(self, f, df, ddf, x0, tol, iter, err):
         self.mensaje = ""
         self.data = []
