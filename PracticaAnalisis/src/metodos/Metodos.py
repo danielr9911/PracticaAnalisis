1b30f61da413ae6989147262c0ad43022b06e857
Carlos Daniel Ruiz <cardaniruiz@gmail.com>
2018-04-21 14:31:11 -0500

    Merge de Newton y Raices Multiples

--- PracticaAnalisis/src/metodos/Metodos.py
+++ PracticaAnalisis/src/metodos/Metodos.py
@@ -83,7 +83,7 @@ class Metodos:
         fila.append(x0)
         fila.append(fx)
         fila.append(dfx)
-        fila.append(ddf)
+        fila.append(ddfx)
         fila.append(0)
         self.data.append(fila)
         while fx != 0 and error > tol and cont < iter:
@@ -102,7 +102,7 @@ class Metodos:
             fila.append(x0)
             fila.append(fx)
             fila.append(dfx)
-            fila.append(ddf)
+            fila.append(ddfx)
             fila.append(error)
             self.data.append(fila)
         if fx == 0:
@@ -114,6 +114,46 @@ class Metodos:
         print(self.mensaje, self.data)
         return (self.mensaje, self.data)
 
-if __name__ == '__main__':
-    Metodos.raicesMultiples(Metodos(None), 'x^4-18*x^2+81', '(4*x^3)-(36*x)', '(12*x^2)-36', -2.5, 0.00000005, 25, 0)
+    def newton(self, f, df, x0, tol, iter, err):
+        mensaje = ""
+        data = []
+        fx = self.funcion(f,x0)
+        dfx = self.funcion(df, x0)
+        cont = 0
+        error = tol + 1
+        fila = []
+        fila.append(cont)
+        fila.append(x0)
+        fila.append(fx)
+        fila.append(dfx)
+        fila.append(0)
+        self.data.append(fila)
+        while fx != 0 and dfx != 0 and error > tol and cont < iter:
+            x1 = x0 - (fx/dfx)
+            fx = self.funcion(f,x1)
+            dfx = self.funcion(df,x1)
+            if err == 0:
+                error = abs(x1 - x0)
+            else:
+                error = abs((x1 - x0) / x1)
+            x0 = x1
+            cont = cont + 1
+            fila = []
+            fila.append(cont)
+            fila.append(x0)
+            fila.append(fx)
+            fila.append(dfx)
+            fila.append(error)
+            self.data.append(fila)
+        if fx == 0:
+            self.mensaje = "%f es una raiz" % x0
+        elif error < tol:
+            self.mensaje = "%f es aproximacion a una raiz con una tolerancia de %f" % (x1, tol)
+        elif dfx == 0:
+            self.mensaje = "%f es una posible raíz múltiple" % (x1)
+        else:
+            self.mensaje = "Fracaso en %d iteraciones" % iter
+        print(self.mensaje, self.data)
+        return (self.mensaje, self.data)
+
 
