6e2ae2df55b3f075b3117295402dd36d15ad6895
Daniel Rendon <dani-9911@hotmail.com>
2018-04-28 15:17:19 -0500

    Python metodos2

---
+++ PracticaAnalisis/src/metodos/Metodos2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from py_expression_eval import Parser
+
+class Metodos2:
+    __gui = None
+    mensaje = ""
+    data = []
+
+    def __init__(self, gui):
+        self.__gui = gui
+
