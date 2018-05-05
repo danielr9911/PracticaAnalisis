81e0bb0119f07233af622aa44ac545cd55cb9f45
Daniel Rendon <dani-9911@hotmail.com>
2018-05-05 12:00:11 -0500

    avance en Pivoteo parcial

--- PracticaAnalisis/src/practicaanalisis/Metodos2.java
+++ PracticaAnalisis/src/practicaanalisis/Metodos2.java
@@ -5,6 +5,8 @@
  */
 package practicaanalisis;
 
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import org.python.core.PyInstance;
 import org.python.core.PyObject;
 import org.python.core.PyArray;
@@ -21,8 +23,9 @@ public class Metodos2 {
     public static Double[][] matizFinal = null;
     public static String resX= "";
     
-    public static void pivoteoParcial(Double[][] a, Double[] b, int n){
-        init();
+    public static void pivoteoParcial(Double[][] a, Double[] b, int n) throws URISyntaxException{
+        //init();
+        /*
         PyArray pa = new PyArray(Double.class,a);
         PyArray pb = new PyArray(Double.class,b);
         PyInteger ptam = new PyInteger(n);
@@ -31,15 +34,32 @@ public class Metodos2 {
         po[1] = pb;
         po[2] = ptam;
         PyObject res = me.invoke("metodoPivoteoParcial", po);
+        */
+        String matrizA = "[";
+        for (Double[] a1 : a) {
+            matrizA = matrizA + Arrays.toString(a1) + ",";
+        }
+        matrizA = matrizA.substring(0,matrizA.length()-1);
+        matrizA = matrizA + "]";
+        String matrizB = Arrays.toString(b);
+        System.out.println(matrizA);
+        System.out.println(Arrays.toString(b));
+        System.out.println(n);
+        String args = n +" "+matrizB+" "+matrizA;
         
+        String ruta = Metodos2.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+        ruta = ruta + "/metodos/pivoteoParcial.py";
+        String comando = "python3 " + ruta + " " + args;
+        System.out.println(comando);
         //Interpretar res
     }
     
     
-    
+    /*
     public static void init(){
         PythonConnection pc = new PythonConnection();  
         pc.execfile("src/metodos/Metodos2.py");  
         me = pc.createClass("Metodos2", "None"); 
     }
+    */
 }
