048157d5b8b91b37dd91049121e866791d902a3e
Daniel Rendon <dani-9911@hotmail.com>
2018-05-27 16:41:54 -0500

    Elimino archivos extra

--- PracticaAnalisis/src/metodos/Lagrange.py
+++ PracticaAnalisis/src/metodos/Lagrange.py
@@ -21,14 +21,23 @@ def main():
 
     resultado = 0
     pol = ""
+    ban = True
     for k in range(n):
         productoria = 1
         termino = ""
         for i in range(n):
             if i!=k:
-                productoria = productoria * (val - x[i]) / (x[k] - x[i])
-                #termino = termino + ("[(x-"+x[i]+")/("+x[k]+"-"+x[i]+")]")
-                termino = termino + "((x-%f)/(%f-%f))" %(x[i],x[k],x[i])
+                if (x[k] - x[i]) == 0.0:
+                    print("Se encontro una division por cero")
+                    print("!")
+                    print("Se encontro una division por cero")
+                    print("!")
+                    print("0")
+                    ban = False
+                else:
+                    productoria = productoria * (val - x[i]) / (x[k] - x[i])
+                    #termino = termino + ("[(x-"+x[i]+")/("+x[k]+"-"+x[i]+")]")
+                    termino = termino + "((x-%f)/(%f-%f))" %(x[i],x[k],x[i])
 
         temp = "L%d(x): " %k
         temp = temp + termino
@@ -38,14 +47,16 @@ def main():
         pol = pol + str(y[k]) + "*" + termino +"\n"
         resultado = resultado + (productoria * y[k])
 
-    print("----------------------------------------")
-    print("POLINOMIO:")
-    pol.replace("\n", "").replace(")(", ")*(")
-    print("P(x): " + pol)
-    print("----------------------------------------")
-    print("RESULTADO:")
-    res = "f(%f) = %f" % (val, resultado)
-    print(res)
-    print("!")
-    print(pol.replace("\n", "").replace(")(", ")*("))
+    if ban:
+        print("----------------------------------------")
+        print("POLINOMIO:")
+        pol.replace("\n", "").replace(")(", ")*(")
+        print("P(x): " + pol)
+        print("----------------------------------------")
+        print("RESULTADO:")
+        res = "f(%f) = %f" % (val, resultado)
+        print(res)
+        print("!")
+        print(pol.replace("\n", "").replace(")(", ")*("))
+        
 main()
