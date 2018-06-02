d6733c5d820eaea1df55add36c2b2b4cd2c02523
lauramv21 <laura_mv21@hotmail.com>
2018-06-02 17:42:14 -0500

    inversa.py

---
+++ PracticaAnalisis/lib/metodos/Inversa.py
@@ -0,0 +1,62 @@
+import numpy as np
+import sys
+from math import *
+
+def calcularInversa(L,U, tam):
+    matrizInversa = np.zeros((tam,tam))
+    Z = np.zeros(tam)
+    X = np.zeros(tam)
+    columna = np.zeros(tam)
+    for i in range(tam):
+        columna = hallarColumnaInversa(i,tam)
+        Z = sustitucionProgresiva(Z, columna, L, tam)
+        X = sustitucionRegresiva(X, Z, U, tam)
+        for j in range(tam):
+            matrizInversa[j][i] = X[j]
+    return matrizInversa
+
+def hallarColumnaInversa(indice, n):
+    col = np.zeros(n)
+    col[indice] = 1
+    return col
+
+
+def sustitucionProgresiva(Z, B, L, n):
+    for i in range(n):
+        sum = 0.0
+        for p in range(i):
+            sum += L[i][p] * Z[p]
+        Z[i] = (B[i] - sum) / L[i][i]
+    return Z
+
+def imprimirMatriz(matriz, tam):
+    linea = ""
+    for i in range(tam):
+        for j in range(tam):
+            if(j != len(matriz)-1):
+                linea += str(matriz[i][j]) + ";"
+            else:
+                linea+= str(matriz[i][j])
+        if(i != len(matriz)-1):
+            linea+=":"
+    print(linea)
+
+def sustitucionRegresiva(X, Z, U, n):
+    for i in range(n - 1, -1, -1):
+        sum = 0.0
+        for p in range(i + 1, n):
+            sum += U[i][p] * X[p]
+        X[i] = (Z[i] - sum) / U[i][i]
+    return X
+
+def main():
+    tam = int(sys.argv[1])
+    sl = sys.argv[2]
+    su = sys.argv[3]
+    L = np.fromstring(sl,dtype=float,sep=",")
+    U = np.fromstring(su,dtype=float,sep=",")
+    inv = calcularInversa(L, U, tam)
+    imprimirMatriz(inv, tam)
+
+
+main()
